d74eac2c708849845c9747f63d0f178d6013a18d
Engr Mohammad Abul Kashem <kashemaust@gmail.com>
2022-02-11 15:18:28 +0600

    error handling done

--- js/banking.js
+++ js/banking.js
@@ -11,29 +11,52 @@ function getUpdatedAcc(accountId, transectionAmount) {
     const currentBalanceAmount = parseFloat(currentAccText.innerText);
     const updatedBalance = currentBalanceAmount + transectionAmount;
     currentAccText.innerText = updatedBalance;
-    return currentAccText;
+};
+//get current balance
+function getCurrentBalance(balanceId) {
+    const balanceTotalText = document.getElementById(balanceId);
+    const currentBalanceAmount = parseFloat(balanceTotalText.innerText);
+    return currentBalanceAmount;
 };
 //updat balance accordingly
 function getBalanceUpdat(amount, isAdd) {
     const balanceTotalText = document.getElementById('balance-total');
-    const balanceAmount = parseFloat(balanceTotalText.innerText);
+    const balanceAmount = getCurrentBalance('balance-total');
     if (isAdd == true) {
         balanceTotalText.innerText = balanceAmount + amount;
     }
     else {
         balanceTotalText.innerText = balanceAmount - amount;
-    }
+    };
 
-}
+};
 document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
-    getUpdatedAcc('deposit-total', depositAmount);
-    getBalanceUpdat(depositAmount, true)
-
+    if (depositAmount > 0) {
+        getUpdatedAcc('deposit-total', depositAmount);
+        getBalanceUpdat(depositAmount, true);
+    }
+    else if (depositAmount < 0) {
+        alert('You can not deposit negative amount!!');
+    }
+    else {
+        alert('Enter a valid Number.!!');
+    };
 });
 document.getElementById('withdraw-btn').addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw-input');
-    getUpdatedAcc('withdraw-total', withdrawAmount);
-    getBalanceUpdat(withdrawAmount, false);
-
-})
+    const currentBalance = getCurrentBalance('balance-total');
+    if (withdrawAmount > 0 && currentBalance >= withdrawAmount) {
+        getUpdatedAcc('withdraw-total', withdrawAmount);
+        getBalanceUpdat(withdrawAmount, false);
+    }
+    else if (withdrawAmount < 0) {
+        alert('You can not withdraw negative amount!!');
+    }
+    else if (currentBalance < withdrawAmount) {
+        alert('Insufficient Fund. Please deposit first.!!!');
+    }
+    else {
+        alert('Enter a valid Number.!!');
+    };
+});
